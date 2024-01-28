aa7782b4520e9920496554501db21184bf4031ae
karannanesha <karannanesha37@gmail.com>
2024-01-28 13:53:22 +0530

    cicd check on prod

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello Shills Professionals CICD testing with branching. 
+      Hello Shills Professionals CICD on prod.
     </div>
   );
 }
